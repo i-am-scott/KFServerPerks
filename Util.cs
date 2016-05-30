814c998b8fba7bd92ae84f5411f540a25b51d12c
Scott <scottdavie564@msn.com>
2016-05-30 11:49:57 +0100

    Basic Logging.

---
+++ Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace KFServerPerks.util
+{
+
+    public static class Logging
+    {
+        public static string logFolder = "./logs/";
+
+        public static void Log( string fileName, string text, bool print = true )
+        {
+            string logtext  = $"[{DateTime.Now}] {text}\n";
+            string filePath = logFolder + fileName;
+
+            try
+            {
+                Directory.CreateDirectory(logFolder);
+                File.AppendAllText(filePath, logtext );
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine($"[ERROR] {e.Message}\n{e.StackTrace}");
+            }
+
+            if (print)
+                Console.WriteLine(logtext);
+        }
+
+        public static void Log( string text, bool print = true )
+        {
+            string date = DateTime.Now.ToShortDateString().Replace( '/', '-' ) + ".log";
+            Log( date, text, print);
+        }
+
+    }
+
+}
