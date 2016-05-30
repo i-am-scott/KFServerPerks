8a0d488c8917d5fbf2d06509e42e7a1b9e0a87bd
Scott <scottdavie564@msn.com>
2016-05-30 11:50:18 +0100

    Logging and renaming.

--- Program.cs
+++ Program.cs
@@ -4,48 +4,34 @@ using System.Linq;
 using System.Threading;
 using System.Net.Sockets;
 using System.Reflection;
+using System.Text;
+using KFServerPerks.util;
 
 namespace KFServerPerks
 {
 
-    public enum ENetID
-    {
-        ID_None = 0,
-        ID_Open,
-        ID_RequestPassword,
-        ID_HeresPassword,
-        ID_ConnectionClosed,
-        ID_PasswordCorrect,
-        ID_NewPlayer,
-        ID_BadNewPlayer,
-        ID_SendPlayerData,
-        ID_UpdatePlayer,
-        ID_KeepAlive
-    }
-
     class Program
     {
+        private static int Port = 5000;
         private static string Password = "nope";
-        private static int    Timeout  = 60;
-        private static int    Port     = 5000;
 
-        private static UdpClient soc        = null;
-        private static IPEndPoint endpoint  = null;
+        private static UdpClient soc = null;
+        private static IPEndPoint endpoint = null;
 
         [AttributeUsage(System.AttributeTargets.Method)]
-        public class ENetIDCommandType : Attribute
+        public class ENetCommandType : Attribute
         {
             public string cmdType;
             public ENetID cmdInt;
 
-            public ENetIDCommandType( string name, ENetID cmd )
+            public ENetCommandType(string name, ENetID cmd)
             {
                 this.cmdType = name;
-                this.cmdInt  = cmd;
+                this.cmdInt = cmd;
             }
         }
 
-        static string ConvertToSteamID32( string steamid64)
+        static string ConvertToSteamID32(string steamid64)
         {
             long sid;
             string steamId = "";
@@ -53,8 +39,8 @@ namespace KFServerPerks
             if (long.TryParse(steamid64, out sid))
             {
                 long accountid = sid - (sid >> 32 << 32);
-                int lastBit    = accountid % 2 == 0 ? 0 : 1;
-                steamId        = "STEAM_0:" + lastBit + ":" + (accountid >> 1).ToString();
+                int lastBit = accountid % 2 == 0 ? 0 : 1;
+                steamId = "STEAM_0:" + lastBit + ":" + (accountid >> 1).ToString();
             }
 
             return steamId;
@@ -62,7 +48,7 @@ namespace KFServerPerks
 
         static void StartListener()
         {
-            soc      = new UdpClient(new IPEndPoint(IPAddress.Any, Port));
+            soc = new UdpClient(new IPEndPoint(IPAddress.Any, Port));
             endpoint = new IPEndPoint(IPAddress.Any, 5000);
 
             while (soc.Client != null)
@@ -71,31 +57,31 @@ namespace KFServerPerks
             soc.Close();
         }
 
-        static void OnMessageReceived(byte[] res )
+        static void OnMessageReceived(byte[] res)
         {
-            byte cmd       = res[0];
-            byte[] data    = new ArraySegment<byte>( res, 1, res.Length - 1 ).ToArray();
+            byte cmd = res[0];
+            byte[] data = new ArraySegment<byte>(res, 1, res.Length - 1).ToArray();
             string encoded = System.Text.Encoding.ASCII.GetString(data);
 
-            Console.WriteLine("Listening on {0}:{1}", endpoint.Address, endpoint.Port );
-            Console.WriteLine("Received information: Type: {0} | Command: {1}", cmd, data.Length );
+            Logging.Log($"Listening on {endpoint.Address}:{endpoint.Port}");
+            Logging.Log($"Received information: Type: {cmd} | Command: {data.Length}");
 
             /* Attempt to map message type to method with the correct attribute. */
             MethodInfo[] methods = typeof(Program).GetMethods()
-                         .Where(m => m.GetCustomAttributes(typeof(ENetIDCommandType), false).Length > 0)
+                         .Where(m => m.GetCustomAttributes(typeof(ENetCommandType), false).Length > 0)
                          .ToArray();
 
             for (int I = 0; I < methods.Length; I++)
             {
-                MethodInfo method        = methods[I];
-                ENetIDCommandType attr   = method.GetCustomAttribute<ENetIDCommandType>();
+                MethodInfo method = methods[I];
+                ENetCommandType attr = method.GetCustomAttribute<ENetCommandType>();
                 string name = attr.cmdType;
-                ENetID id   = attr.cmdInt;
+                ENetID id = attr.cmdInt;
 
                 if (((int)id == (int)cmd))
                 {
-                    Console.WriteLine("[{0}] {1} {2}", DateTime.Now, name, encoded );
-                    method.Invoke( null, ( new object[] {
+                    Logging.Log($"[{DateTime.Now}] {name} {encoded}");
+                    method.Invoke(null, (new object[] {
                         encoded
                     }));
                     break;
@@ -103,25 +89,25 @@ namespace KFServerPerks
             }
         }
 
-        static void SendMessage( ENetID type, string message = "" )
+        static void SendMessage(ENetID type, string message = "")
         {
-            byte[] msgarr       = System.Text.Encoding.ASCII.GetBytes( message );
+            byte[] msgarr = System.Text.Encoding.ASCII.GetBytes(message);
             byte[] broadcastmsg = new byte[msgarr.Length + 1];
 
             // >__>
-            broadcastmsg[0]     = (byte)((int)type);
-            broadcastmsg        = broadcastmsg.Concat(msgarr).ToArray<byte>();
+            broadcastmsg[0] = (byte)((int)type);
+            broadcastmsg = broadcastmsg.Concat(msgarr).ToArray<byte>();
 
             soc.Send(broadcastmsg, broadcastmsg.Length, endpoint);
         }
 
-        [ENetIDCommandType( "ConnectionStart", ENetID.ID_Open )]
-        public static void ConnectionStart( string data )
+        [ENetCommandType("ConnectionStart", ENetID.ID_Open)]
+        public static void ConnectionStart(string data)
         {
             SendMessage(ENetID.ID_RequestPassword);
         }
 
-        [ENetIDCommandType("CheckPassword", ENetID.ID_HeresPassword)]
+        [ENetCommandType("CheckPassword", ENetID.ID_HeresPassword)]
         public static void CheckPassword(string data)
         {
             if (data == Password)
@@ -130,31 +116,43 @@ namespace KFServerPerks
                 return;
             }
             else
-            { 
+            {
                 SendMessage(ENetID.ID_ConnectionClosed);
                 return;
             }
         }
-     
-        [ENetIDCommandType("KeepAlive", ENetID.ID_KeepAlive)]
-        public static void KeepAlive( string data )
+
+        [ENetCommandType("KeepAlive", ENetID.ID_KeepAlive)]
+        public static void KeepAlive(string data)
         {
-            Console.WriteLine("Keep alive request.");
+            Logging.Log("Keep alive request.");
         }
 
-        [ENetIDCommandType("NewPlayer", ENetID.ID_NewPlayer)]
+        [ENetCommandType("NewPlayer", ENetID.ID_NewPlayer)]
         public static void NewPlayer(string data)
         {
-            string[] tbl     = data.Split(new char[] { '*' });
-            string steamid64 = ConvertToSteamID32( tbl[0] );
-            string name      = tbl[1];
+            string[] tbl = data.Split(new char[] { '*' });
+            string steamid64 = ConvertToSteamID32(tbl[0]);
+            string name = tbl[1];
 
-            Console.WriteLine( "Player received: {0} ({1})", name, steamid64 );
+            Logging.Log($"Player received: {name} ({steamid64})");
+        }
+
+        [ENetCommandType("UpdatePlayer", ENetID.ID_UpdatePlayer)]
+        public static void UpdatePlayer(string data)
+        {
+            string[] dataArr = data.Split(new char[] { '|' });
+            int playerId = int.Parse(dataArr[0]);
+            string[] stats = dataArr[1].Split(new char[] { ',' });
+
+            Stats playerstats = new Stats(playerId, stats);
+
         }
 
         static void Main(string[] args)
         {
             Console.Title = "KillingFloor Perk Server 0.1";
+            Logging.Log($"Listening on Port {Port} with Password '{Password}'.");
 
             Thread thr = new Thread(new ThreadStart(StartListener));
             thr.IsBackground = true;
